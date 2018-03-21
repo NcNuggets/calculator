56df4b41fc03545ec163122de884b9f1250e5b02
Nick <pucillo.nick@gmail.com>
2018-03-20 23:23:29 -0400

    mostly finished operate function and basic math for long strings.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,47 @@
+//-----TO DO LIST-----//
+
+// need to remove duplicate operators with regex
+// need to add a backspace functionality (pops off the last input added to array)
+
 //-----INITIAL DECLARATIONS-----//
 let displayVal = 0;
 let input = [];
-let output = [];
+
+//-----RUNS ACCORDING TO PEMDAS (I HOPE)-----//
+
+function operate(array) {
+  let outputArr = array;
+  console.log('operatestart: ' + outputArr);
+
+  outputArr = calcPrepare(outputArr);
+  console.log('afterprep: ' + outputArr);
+  outputArr = multiply(outputArr);
+  console.log('aftermult: ' + outputArr);
+  outputArr = divide(outputArr);
+  console.log('afterdiv: ' + outputArr);
+  outputArr = add(outputArr);
+  console.log('afteradd: ' + outputArr);
+  outputArr = subtract(outputArr);
+  console.log('aftersub: ' + outputArr);
+  outputArr = negatives(outputArr);
+  console.log('afternegs: ' + outputArr);
+
+  input = outputArr;
+  console.log('output is new input: ' + input);
+
+  displayVal = Math.round(Number(outputArr), -3);
+  displayAnswer(displayVal);
+}
+
+//-----PREPARES INPUT STRING TO BE BROKEN INTO NUMBERS AND OPERATORS-----//
+
+function calcPrepare(array) {
+  let prepared = Array.from(array);
+  prepared = prepared.join('');
+  prepared = prepared.match(/(\-?[0-9.]+)|([\+\-\*\/])/g);
+
+  return prepared;
+}
 
 //-----OPERATIONS-----//
 
@@ -9,38 +49,50 @@ function multiply(array) {
   let inputArr = Array.from(array);
   for (i = 0; i < inputArr.length; i++) {
     if (inputArr[i] === '*') {
-      console.log(inputArr.splice(i - 1, 3, inputArr[i-1] * inputArr[i+1]));
+      inputArr.splice(i - 1, 3, Number(inputArr[i-1]) * Number(inputArr[i+1]));
     }
   }
   return inputArr;
 }
 
-function divide(num1, num2) {
-  return num1 / num2;
+function divide(array) {
+  let inputArr = Array.from(array);
+  for (i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] === '/') {
+      inputArr.splice(i - 1, 3, Number(inputArr[i-1]) / Number(inputArr[i+1]));
+    }
+  }
+  return inputArr;
 }
 
-function add(num1, num2) {
-  return num1 + num2;
+function add(array) {
+  let inputArr = Array.from(array);
+  for (i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] === '+') {
+      inputArr.splice(i - 1, 3, Number(inputArr[i-1]) + Number(inputArr[i+1]));
+    }
+  }
+  return inputArr;
 }
 
-function subtract(num1, num2) {
-  return num1 - num2;
+function subtract(array) {
+  let inputArr = Array.from(array);
+  for (i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] === '-') {
+      inputArr.splice(i - 1, 3, Number(inputArr[i-1]) - Number(inputArr[i+1]));
+    }
+  }
+  return inputArr;
 }
 
-function operate(operator, n1, n2) {
-
-  displayVal = "input";
-  displayAnswer(displayVal);
-}
-
-//-----PREPARES INPUT STRING TO BE BROKEN INTO NUMBERS AND OPERATORS-----//
-
-function calcPrepare(array) {
-  let prepared = array;
-  prepared = prepared.join('');
-  prepared = prepared.match(/(\-?[0-9.]+)|([\+\-\*\/])/g);
-
-  return input = prepared;
+function negatives(array) {
+  let inputArr = Array.from(array);
+  for (i = 0; i < inputArr.length; i++) {
+    if (inputArr[i] < 0) {
+      inputArr.splice(i - 1, 2, Number(inputArr[i-1]) + Number(inputArr[i]));
+    }
+  }
+  return inputArr;
 }
 
 //-----BUTTONS FOR CALCULATOR-----//
@@ -80,7 +132,6 @@ function displayAnswer(result) {
 function reset() {
   displayVal = 0;
   input = [];
-  output = [];
   return displayAnswer(displayVal);
 }
 
